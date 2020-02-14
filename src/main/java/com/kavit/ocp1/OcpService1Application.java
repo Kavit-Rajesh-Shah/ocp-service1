c9f0fc964b8354344c8c77f5a7fb3a09a1785218
kavit.shah <kavitsh@gmail.com>
2020-02-14 11:10:11 +0100

    Initial Commit

---
+++ src/main/java/com/kavit/ocp1/OcpService1Application.java
@@ -0,0 +1,13 @@
+package com.kavit.ocp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcpService1Application.class, args);
+	}
+
+}
